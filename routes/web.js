bec712b324814cd15715f52c94205276ef8d5585
PFZM <pfzm@hotmail.com>
2021-11-12 20:17:34 +1100

    fix issue

--- routes/web.js
+++ routes/web.js
@@ -3,17 +3,17 @@ const path = require("path");
 
 // GET route for homepage
 webRoutes.get("/", (req, res) => {
-  res.sendFile(path.join(process.cwd(), "public/index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 // GET route for notes page
 webRoutes.get("/notes", (req, res) => {
-  res.sendFile(path.join(process.cwd(), "public/notes.html"));
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
 // wildcard route to direct users to homepage
 webRoutes.get("*", (req, res) => {
-  res.sendFile(path.join(process.cwd(), "public/index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports = webRoutes;
